da5ddf9359bbdddd7512d6fe3702728987eb2dc0
Abdujabborov Abdurahmon <abdujabborovabdurahmon13@gmail.com>
2025-11-18 15:14:07 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const name1 = prompt("Ismingni yoz:");
+const name2 = prompt("Ismingni qaytadan yoz :");
+
+if (name1 === name2) {
+    console.log("Raxmat malades");
+    alert("Raxmat malades");
+} else {
+    console.log("Notog'ri g'alchamisan !");
+    alert("Notog'ri g'alchamisan !");
+}
